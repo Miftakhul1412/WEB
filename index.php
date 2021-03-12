5f7151dc68de0e77c6e6b949352b7b31fff4845d
Miftakhul1412 <mifta.ervi12@gmail.com>
2021-03-12 19:48:03 +0700

    percobaan

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mifta</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="main">
+			<ul>
+				<li><a href="home.php" class="action">HOME</a></li>
+				<li><a href="service.php" >SERVICE</a></li>
+			</ul>
+		</div>
+	</header>
+	<div class="tittle">
+		<h2>SELAMAT DATANG</h2>
+		<h5>MIFTAKHUL ERVYANTI SANTOSA - 193110001</h5>
+	</div>
+</body>
+</html>
